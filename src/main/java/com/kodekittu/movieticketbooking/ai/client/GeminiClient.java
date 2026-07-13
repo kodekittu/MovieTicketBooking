ea2f383de38d034a688b4484bdd32c7cf026a52d
Kalpana Kumari <kalpana@Kalpanas-MacBook-Air.local>
2026-07-13 22:25:09 +0530

    AI integration setup done

--- src/main/java/com/kodekittu/movieticketbooking/ai/client/GeminiClient.java
+++ src/main/java/com/kodekittu/movieticketbooking/ai/client/GeminiClient.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kodekittu.movieticketbooking.ai.config.GeminiProperties;
 import com.kodekittu.movieticketbooking.ai.dto.LLMRequest;
 import com.kodekittu.movieticketbooking.ai.dto.LLMResponse;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
@@ -23,6 +25,15 @@ public class GeminiClient implements LLMClient {
     private final RestClient restClient;
     private final GeminiProperties properties;
     private final ObjectMapper objectMapper;
+
+    @PostConstruct
+    public void init() {
+
+        log.info("Gemini Base URL : {}", properties.getBaseUrl());
+        log.info("Gemini Model : {}", properties.getModel());
+        log.info("Embedding Model : {}", properties.getEmbeddingModel());
+
+    }
 
     @Override
     public LLMResponse generateResponse(LLMRequest request) {
--- src/main/java/com/kodekittu/movieticketbooking/ai/controller/AIController.java
+++ src/main/java/com/kodekittu/movieticketbooking/ai/controller/AIController.java
@@ -1,9 +1,9 @@
 package com.kodekittu.movieticketbooking.ai.controller;
 
+import com.kodekittu.movieticketbooking.ai.client.GeminiClient;
+import com.kodekittu.movieticketbooking.ai.client.PineconeClient;
+import com.kodekittu.movieticketbooking.ai.dto.*;
 import com.kodekittu.movieticketbooking.ai.service.AIChatService;
-import com.kodekittu.movieticketbooking.ai.dto.ChatRequest;
-import com.kodekittu.movieticketbooking.ai.dto.ChatResponse;
-import com.kodekittu.movieticketbooking.ai.dto.RecommendationResponse;
 import com.kodekittu.movieticketbooking.ai.service.AIRecommendationService;
 import com.kodekittu.movieticketbooking.ai.service.MovieEmbeddingService;
 import com.kodekittu.movieticketbooking.repository.RecommendationRepository;
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -25,6 +26,8 @@ public class AIController {
     private final AIChatService aiChatService;
     private final AIRecommendationService recommendationService;
     private final MovieEmbeddingService embeddingService;
+    private final GeminiClient geminiClient;
+    private final PineconeClient pineconeClient;
 
     @PostMapping("/chat")
     public ResponseEntity<ChatResponse> chat(
@@ -45,13 +48,75 @@ public class AIController {
     }
 
     @PostMapping("/reindex")
-    @PreAuthorize("hasRole('ADMIN')")
+    // @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> reindex() {
 
         embeddingService.indexAllMovies();
 
         return ResponseEntity.ok("Movie indexing completed.");
 
+    }
+
+
+    // endpoint for testing gemini integration
+
+    @PostMapping("/test-gemini")
+    public String testGemini() {
+
+        return geminiClient.generateResponse(
+                LLMRequest.builder()
+                        .prompt("Say Hello from Gemini")
+                        .build()
+        ).getResponse();
+
+    }
+
+    @GetMapping("/test-embedding")
+    public List<Double> embedding() {
+
+        return geminiClient.generateEmbedding(
+                "Interstellar is a science fiction movie."
+        );
+
+    }
+
+    // endpoint for testing pinecone integration
+
+    @PostMapping("/test-pinecone")
+    public ResponseEntity<String> testPinecone() {
+
+        List<Double> embedding = geminiClient.generateEmbedding(
+                "Interstellar is a science fiction movie."
+        );
+
+        pineconeClient.upsert(
+                UUID.randomUUID().toString(),
+                embedding,
+                Map.of(
+                        "title", "Interstellar",
+                        "genre", "Sci-Fi",
+                        "language", "English",
+                        "movieId", UUID.randomUUID().toString()
+                )
+        );
+
+        return ResponseEntity.ok("Inserted into Pinecone successfully");
+    }
+
+    @GetMapping("/test-search")
+    public ResponseEntity<List<VectorSearchResult>> testSearch() {
+
+        List<Double> embedding = geminiClient.generateEmbedding(
+                "A science fiction movie about astronauts in space"
+        );
+
+        List<VectorSearchResult> results =
+                pineconeClient.similaritySearch(
+                        embedding,
+                        5
+                );
+
+        return ResponseEntity.ok(results);
     }
 
 }
